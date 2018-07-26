39e50053bb8fc926ed4eba83f0a5352bce1eb1db
Aubin COSTE <dominiqueduri@gmail.com>
2018-07-26 10:52:58 +0200

    premier commit

---
+++ src/main/java/fr/gtm/projetproxibanquev2ws/service/TestVirement.java
@@ -0,0 +1,22 @@
+package fr.gtm.projetproxibanquev2ws.service;
+
+import org.junit.Test;
+
+public class TestVirement {
+	
+	@Test
+	public void testSoustractionCompteDebiteur() {
+		System.out.println("true");
+	}
+	
+	@Test
+	public void testAdditionCompteCrediteur() {
+		System.out.println("true");
+	}
+	
+	@Test
+	public void testDecouvertCompteDebiteur() {
+		System.out.println("true");
+	}
+	
+}
